a53ccae9e596afff458a91cdda23eb6d5d637acc
Spock <spock@example.com>
2023-11-21 07:04:10 +0100

    Contenuto salvato

--- genera-sito.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-umask 022
-umask -S
-cd /opt/neoassunti2024-cms 
-export USE_STATIC_TRUST_PROXIES=true
-php please ssg:generate --workers=4
-chmod -R g+rwX /opt/neoassunti2024-cms/storage/app/static
-
