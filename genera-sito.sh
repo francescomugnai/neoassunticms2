23a9885fe9ec5340a384ae6bc75fe9d35c5b4863
Administrator <admin@example.com>
2023-11-21 07:02:11 +0000

    aggiunto push dei file dei contenuti nel repository gitlab

--- genera-sito.sh
+++ genera-sito.sh
@@ -2,6 +2,7 @@
 umask 022
 umask -S
 cd /opt/neoassunti2024-cms 
+php please git:commit
 export USE_STATIC_TRUST_PROXIES=true
 php please ssg:generate --workers=4
 chmod -R g+rwX /opt/neoassunti2024-cms/storage/app/static
